ab8ef0fd0498fe418e8fb7dd250986ad8ad27064
alejomvides <alejo.vides@mi.unc.edu.ar>
2025-05-24 20:10:07 -0300

    Update circulo.s

--- ejercicio1/circulo.s
+++ ejercicio1/circulo.s
@@ -1,8 +1,7 @@
 .text
 
 circulo:
-	//centro (cx, cy) y un radio (r)
-
+	// Centro (cx, cy) y un radio (r)
     mov x20, x0
     mov x21, x1
     mov x22, x2
@@ -10,34 +9,33 @@ circulo:
     mov x24, x4
 
     mul x25, x23, x23
-
-	//loop Y desde 0 hasta SCREEN_HEIGH
-	mov x5, 0	//y = 0
+    
+    	//loop Y desde 0 hasta SCREEN_HEIGH
+    mov x5, 0		// y = 0
 
 y_loop:
 	cmp x5, 640
 	b.ge draw_done
-
-	//loop x desde 0 hasta SCREEN_WIDTH
-	mov x6, 0	//x = 0
+		// loop x desde 0 hasta SCREEN_WIDTH
+	mov x6, 0	// x = 0
 
 x_loop:
 	cmp x6, 480
 	b.ge next_y
 
-	//dx = x - cx, dy = y - cy
+		//dx = x - cx, dy = y - cy
 	sub x7, x6, x1
 	sub x8, x5, x2
 	mul x7, x7, x7
 	mul x8, x8, x8
 	add x9, x7, x8
 
-	//cmp con r^2
+		// cmp con r^2
 	cmp x9, x25
-	b.gt skip_pixel 	//
-
-	//calcular la direccion del pixel
-	// offset = (y * SCREEN_WIDTH + x) * 4
+	b.gt skip_pixel 
+ 
+		// calcular la direccion del pixel
+		// offset = (y * SCREEN_WIDTH + x) * 4
 	mov x10, 640
 	mul x11, x5, x10
 	add x11, x11, x6
