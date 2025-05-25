7e6af88665a048b1ad1716fd3d1696f4d3aa4e62
manug859 <manu.gomez859@gmail.com>
2025-05-25 01:33:08 -0300

    Mejorar la legibilidad del código en cuerpo.s al alinear los comentarios y agregar espacios en blanco.

--- ejercicio1/cuerpo.s
+++ ejercicio1/cuerpo.s
@@ -3,28 +3,31 @@
 .globl cuerpo
 
 cuerpo:
-            // Parte de abajo contorno negro
+    // Parte de abajo contorno negro
     mov x0, x20 //framebuffer base
     mov x1, 320 //cx1
     mov x2, 350 //cy1
     mov x3, 95  //r
     mov x4, 0x00000000  //color
     bl circulo
-            // Parte de abajo color blanco
+    
+    // Parte de abajo color blanco
     mov x0, x20
     mov x1, 320
     mov x2, 350
     mov x3, 90
     mov x4, 0xFFFFFFFF
     bl circulo
-            // Cabeza contorno negro
+    
+    // Cabeza contorno negro
     mov x0, x20
     mov x1, 320
     mov x2, 240
     mov x3, 75
     mov x4, 0x0000000
     bl circulo
-            // Cabeza color blanco
+    
+    // Cabeza color blanco
     mov x0, x20
     mov x1, 320
     mov x2, 240
