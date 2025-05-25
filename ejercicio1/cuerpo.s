914fd8c8b40adb6df7c4dcd9027ccbab59c12ecd
manug859 <manu.gomez859@gmail.com>
2025-05-25 01:35:42 -0300

    Alinear los comentarios y corregir las coordenadas en cuerpo.s para mejorar la legibilidad y precisión del dibujo.

--- ejercicio1/cuerpo.s
+++ ejercicio1/cuerpo.s
@@ -4,34 +4,34 @@
 
 cuerpo:
     // Parte de abajo contorno negro
-    mov x0, x20 //framebuffer base
-    mov x1, 320 //cx1
-    mov x2, 350 //cy1
-    mov x3, 95  //r
+    mov x0, x20     //framebuffer base
+    mov x1, 320     //cx1
+    mov x2, 408     //cy1
+    mov x3, 196     //r
     mov x4, 0x00000000  //color
     bl circulo
     
     // Parte de abajo color blanco
     mov x0, x20
     mov x1, 320
-    mov x2, 350
-    mov x3, 90
+    mov x2, 408
+    mov x3, 189
     mov x4, 0xFFFFFFFF
     bl circulo
     
     // Cabeza contorno negro
     mov x0, x20
     mov x1, 320
-    mov x2, 240
-    mov x3, 75
+    mov x2, 177
+    mov x3, 154
     mov x4, 0x0000000
     bl circulo
     
     // Cabeza color blanco
     mov x0, x20
     mov x1, 320
-    mov x2, 240
-    mov x3, 70
+    mov x2, 177
+    mov x3, 147
     mov x4, 0xFFFFFFFF
     bl circulo
 
