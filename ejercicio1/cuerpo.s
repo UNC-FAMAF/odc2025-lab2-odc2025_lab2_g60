a35a64d3726b20096e96b24b680f495bcf9ab4b1
manug859 <manu.gomez859@gmail.com>
2025-05-25 01:54:06 -0300

    cambios en algunos valores del cuerpo

--- ejercicio1/cuerpo.s
+++ ejercicio1/cuerpo.s
@@ -3,19 +3,19 @@
 .globl cuerpo
 
 cuerpo:
-    // Parte de abajo contorno negro
-    mov x0, x20     //framebuffer base
-    mov x1, 320     //cx1
-    mov x2, 408     //cy1
-    mov x3, 196     //r
+    // Circulo inferior [contorno]
+    mov x0, x20         //framebuffer base
+    mov x1, 320         //cx1
+    mov x2, 338         //cy1
+    mov x3, 97          //r
     mov x4, 0x00000000  //color
     bl circulo
     
-    // Parte de abajo color blanco
+    // Circulo inferior [color blanco]
     mov x0, x20
     mov x1, 320
-    mov x2, 408
-    mov x3, 189
+    mov x2, 338
+    mov x3, 90
     mov x4, 0xFFFFFFFF
     bl circulo
     
@@ -23,7 +23,7 @@ cuerpo:
     mov x0, x20
     mov x1, 320
     mov x2, 177
-    mov x3, 154
+    mov x3, 77
     mov x4, 0x0000000
     bl circulo
     
@@ -31,7 +31,7 @@ cuerpo:
     mov x0, x20
     mov x1, 320
     mov x2, 177
-    mov x3, 147
+    mov x3, 70
     mov x4, 0xFFFFFFFF
     bl circulo
 
