5d380909addb0f4d217a2138ca23f6df38f60f06
nicolasg-unc <nicolasgerbaudo1@mi.unc.edu.ar>
2025-05-26 21:47:45 -0300

    botones, boca y tercer circulo añadido

--- ejercicio1/cuerpo.s
+++ ejercicio1/cuerpo.s
@@ -6,15 +6,31 @@ cuerpo:
     // Circulo inferior [contorno]
     mov x0, x20         //framebuffer base
     mov x1, 320         //cx1
-    mov x2, 338         //cy1
-    mov x3, 97          //r
+    mov x2, 385         //cy1
+    mov x3, 110         //r
     mov x4, 0x00000000  //color
     bl circulo
     
     // Circulo inferior [color blanco]
     mov x0, x20
     mov x1, 320
-    mov x2, 338
+    mov x2, 385
+    mov x3, 102
+    mov x4, 0xFFFFFFFF
+    bl circulo
+
+    // Circulo del medio [contorno]
+    mov x0, x20         //framebuffer base
+    mov x1, 320         //cx1
+    mov x2, 248         //cy1
+    mov x3, 97          //r
+    mov x4, 0x00000000  //color
+    bl circulo
+    
+    // Circulo del medio [color blanco]
+    mov x0, x20
+    mov x1, 320
+    mov x2, 248
     mov x3, 90
     mov x4, 0xFFFFFFFF
     bl circulo
@@ -22,7 +38,7 @@ cuerpo:
     // Cabeza contorno negro
     mov x0, x20
     mov x1, 320
-    mov x2, 177
+    mov x2, 115
     mov x3, 77
     mov x4, 0x0000000
     bl circulo
@@ -30,9 +46,105 @@ cuerpo:
     // Cabeza color blanco
     mov x0, x20
     mov x1, 320
-    mov x2, 177
+    mov x2, 115
     mov x3, 70
     mov x4, 0xFFFFFFFF
+    bl circulo
+
+    // Boton 1
+    mov x0, x20
+    mov x1, 320
+    mov x2, 375
+    mov x3, 9
+    mov x4, 0x0000000
+    bl circulo
+
+    // Boton 2
+    mov x0, x20
+    mov x1, 320
+    mov x2, 410
+    mov x3, 9
+    mov x4, 0x0000000
+    bl circulo
+
+    // Boton 3
+    mov x0, x20
+    mov x1, 320
+    mov x2, 445
+    mov x3, 9
+    mov x4, 0x0000000
+    bl circulo
+
+    // Boton 4
+    mov x0, x20
+    mov x1, 320
+    mov x2, 280
+    mov x3, 9
+    mov x4, 0x0000000
+    bl circulo
+
+    // Boton 5
+    mov x0, x20
+    mov x1, 320
+    mov x2, 225
+    mov x3, 9
+    mov x4, 0x0000000
+    bl circulo
+
+    // Ojo izq
+    mov x0, x20
+    mov x1, 295
+    mov x2, 100
+    mov x3, 9
+    mov x4, 0x0000000
+    bl circulo
+
+    // Ojo der
+    mov x0, x20
+    mov x1, 345
+    mov x2, 100
+    mov x3, 9
+    mov x4, 0x0000000
+    bl circulo
+
+    // Boca 1
+    mov x0, x20
+    mov x1, 358
+    mov x2, 150
+    mov x3, 8
+    mov x4, 0x0000000
+    bl circulo
+
+    // Boca 2
+    mov x0, x20
+    mov x1, 340
+    mov x2, 160
+    mov x3, 8
+    mov x4, 0x0000000
+    bl circulo
+
+    // Boca 3
+    mov x0, x20
+    mov x1, 320
+    mov x2, 165
+    mov x3, 8
+    mov x4, 0x0000000
+    bl circulo
+
+    // Boca 4
+    mov x0, x20
+    mov x1, 300
+    mov x2, 160
+    mov x3, 8
+    mov x4, 0x0000000
+    bl circulo
+
+    // Boca 5
+    mov x0, x20
+    mov x1, 282
+    mov x2, 150
+    mov x3, 8
+    mov x4, 0x0000000
     bl circulo
 
     RET	
