fc2f1c6a98e4c1bf4dfade0d7f48786e9131a6b3
alejomvides <alejo.vides@mi.unc.edu.ar>
2025-05-28 17:49:04 -0300

    modularizacion del copo de nieve
    podemos replicar el copo tantas veces como haga falta

--- ejercicio1/copo.s
+++ ejercicio1/copo.s
@@ -4,230 +4,165 @@
     .globl copo
 
 copo:
-/* 
-    mov x0, x20
-    mov x1, 1          //x_1
-    mov x2, 8           //y_1
-    mov x3, 21          //x_2
-    mov x4, 14          //y_2
-    ldr x7, =BLANCO     //color
+    STR X30, [SP, #40]  // Se guarda el link register
+    ldr x7, =BLANCO     // Todo se pinta de blanco
+    mov x0, x20         // Seteo el framebuffer
+
+    add x1, x9, 7
+    add x2, x10, 14
+    add x3, x9, 20
+    add x4, x10, 20
     bl rectangulo
 
-    mov x0, x20
-    mov x1, 8           //x_1
-    mov x2, 1           //y_1
-    mov x3, 14          //x_2
-    mov x4, 21          //y_2
-    ldr x7, =BLANCO     //color
-    bl rectangulo
-*/
-    mov x0, x20
-    mov x1, 7           //x_1
-    mov x2, 14           //y_1
-    mov x3, 20          //x_2
-    mov x4, 20          //y_2
-    ldr x7, =BLANCO     //color
+    add x1, x9, 14
+    add x2, x10, 7
+    add x3, x9, 20
+    add x4, x10, 13
     bl rectangulo
 
-    mov x0, x20
-    mov x1, 14           //x_1
-    mov x2, 7           //y_1
-    mov x3, 20          //x_2
-    mov x4, 13          //y_2
-    ldr x7, =BLANCO     //color
+    add x1, x9, 21
+    add x2, x10, 21
+    add x3, x9, 27
+    add x4, x10, 27
     bl rectangulo
 
-    mov x0, x20
-    mov x1, 21           //x_1
-    mov x2, 21          //y_1
-    mov x3, 27          //x_2
-    mov x4, 27          //y_2
-    ldr x7, =BLANCO     //color
-    bl rectangulo
-    
-    mov x0, x20
-    mov x1, 28           //x_1
-    mov x2, 0           //y_1
-    mov x3, 34           //x_2
-    mov x4, 6           //y_2
-    ldr x7, =BLANCO      //color
+    add x1, x9, 28
+    add x2, x10, 0
+    add x3, x9, 34
+    add x4, x10, 6
     bl rectangulo
 
-    mov x0, x20
-    mov x1, 42           //x_1
-    mov x2, 0           //y_1
-    mov x3, 48           //x_2
-    mov x4, 6          //y_2
-    ldr x7, =BLANCO      //color
+    add x1, x9, 42
+    add x2, x10, 0
+    add x3, x9, 48
+    add x4, x10, 6
     bl rectangulo
 
-    mov x0, x20
-    mov x1, 35          //x_1
-    mov x2, 7          //y_1
-    mov x3, 41           //x_2
-    mov x4, 27           //y_2
-    ldr x7, =BLANCO      //color
+    add x1, x9, 35
+    add x2, x10, 7
+    add x3, x9, 41
+    add x4, x10, 27
     bl rectangulo
 
-    mov x0, x20
-    mov x1, 49           //x_1
-    mov x2, 21           //y_1
-    mov x3, 55           //x_2
-    mov x4, 27           //y_2
-    ldr x7, =BLANCO      //color
+    add x1, x9, 49
+    add x2, x10, 21
+    add x3, x9, 55
+    add x4, x10, 27
     bl rectangulo
 
-    mov x0, x20
-    mov x1, 56           //x_1
-    mov x2, 7           //y_1
-    mov x3, 62           //x_2
-    mov x4, 20           //y_2
-    ldr x7, =BLANCO      //color
+    add x1, x9, 56
+    add x2, x10, 7
+    add x3, x9, 62
+    add x4, x10, 20
     bl rectangulo
 
-    mov x0, x20
-    mov x1, 63           //x_1
-    mov x2, 14           //y_1
-    mov x3, 69           //x_2
-    mov x4, 20           //y_2
-    ldr x7, =BLANCO      //color
+    add x1, x9, 63
+    add x2, x10, 14
+    add x3, x9, 69
+    add x4, x10, 20
     bl rectangulo
 
-    mov x0, x20
-    mov x1, 28           //x_1
-    mov x2, 28           //y_1
-    mov x3, 48           //x_2
-    mov x4, 34           //y_2
-    ldr x7, =BLANCO      //color
+    add x1, x9, 28
+    add x2, x10, 28
+    add x3, x9, 48
+    add x4, x10, 34
     bl rectangulo
 
-    mov x0, x20
-    mov x1, 7           //x_1
-    mov x2, 35           //y_1
-    mov x3, 34           //x_2
-    mov x4, 41           //y_2
-    ldr x7, =BLANCO      //color
+    add x1, x9, 7
+    add x2, x10, 35
+    add x3, x9, 34
+    add x4, x10, 41
     bl rectangulo
 
-    mov x0, x20
-    mov x1, 42           //x_1
-    mov x2, 35           //y_1
-    mov x3, 69           //x_2
-    mov x4, 41           //y_2
-    ldr x7, =BLANCO      //color
+    add x1, x9, 42
+    add x2, x10, 35
+    add x3, x9, 69
+    add x4, x10, 41
     bl rectangulo
 
-    mov x0, x20
-    mov x1, 28           //x_1
-    mov x2, 42           //y_1
-    mov x3, 48           //x_2
-    mov x4, 48           //y_2
-    ldr x7, =BLANCO      //color
+    add x1, x9, 28
+    add x2, x10, 42
+    add x3, x9, 48
+    add x4, x10, 48
     bl rectangulo
 
-    mov x0, x20
-    mov x1, 0           //x_1
-    mov x2, 28           //y_1
-    mov x3, 6           //x_2
-    mov x4, 34           //y_2
-    ldr x7, =BLANCO      //color
+    add x1, x9, 0
+    add x2, x10, 28
+    add x3, x9, 6
+    add x4, x10, 34
     bl rectangulo
 
-    mov x0, x20
-    mov x1, 0           //x_1
-    mov x2, 42           //y_1
-    mov x3, 6           //x_2
-    mov x4, 48           //y_2
-    ldr x7, =BLANCO      //color
+    add x1, x9, 0
+    add x2, x10, 42
+    add x3, x9, 6
+    add x4, x10, 48
     bl rectangulo
 
-    mov x0, x20
-    mov x1, 70           //x_1
-    mov x2, 28           //y_1
-    mov x3, 76           //x_2
-    mov x4, 34           //y_2
-    ldr x7, =BLANCO      //color
+    add x1, x9, 70
+    add x2, x10, 28
+    add x3, x9, 76
+    add x4, x10, 34
     bl rectangulo
 
-    mov x0, x20
-    mov x1, 70           //x_1
-    mov x2, 42           //y_1
-    mov x3, 76           //x_2
-    mov x4, 48           //y_2
-    ldr x7, =BLANCO      //color
+    add x1, x9, 70
+    add x2, x10, 42
+    add x3, x9, 76
+    add x4, x10, 48
     bl rectangulo
 
-    mov x0, x20
-    mov x1, 21           //x_1
-    mov x2, 49           //y_1
-    mov x3, 27           //x_2
-    mov x4, 55           //y_2
-    ldr x7, =BLANCO      //color
+    add x1, x9, 21
+    add x2, x10, 49
+    add x3, x9, 27
+    add x4, x10, 55
     bl rectangulo
 
-    mov x0, x20
-    mov x1, 49           //x_1
-    mov x2, 49           //y_1
-    mov x3, 55           //x_2
-    mov x4, 55           //y_2
-    ldr x7, =BLANCO      //color
+    add x1, x9, 49
+    add x2, x10, 49
+    add x3, x9, 55
+    add x4, x10, 55
     bl rectangulo
 
-    mov x0, x20
-    mov x1, 35           //x_1
-    mov x2, 49           //y_1
-    mov x3, 41           //x_2
-    mov x4, 69           //y_2
-    ldr x7, =BLANCO      //color
+    add x1, x9, 35
+    add x2, x10, 49
+    add x3, x9, 41
+    add x4, x10, 69
     bl rectangulo
 
-    mov x0, x20
-    mov x1, 7           //x_1
-    mov x2, 56           //y_1
-    mov x3, 20           //x_2
-    mov x4, 62           //y_2
-    ldr x7, =BLANCO      //color
+    add x1, x9, 7
+    add x2, x10, 56
+    add x3, x9, 20
+    add x4, x10, 62
     bl rectangulo
 
-    mov x0, x20
-    mov x1, 14           //x_1
-    mov x2, 63           //y_1
-    mov x3, 20           //x_2
-    mov x4, 69           //y_2
-    ldr x7, =BLANCO      //color
+    add x1, x9, 14
+    add x2, x10, 63
+    add x3, x9, 20
+    add x4, x10, 69
     bl rectangulo
 
-    mov x0, x20
-    mov x1, 56           //x_1
-    mov x2, 56           //y_1
-    mov x3, 69           //x_2
-    mov x4, 62           //y_2
-    ldr x7, =BLANCO      //color
+    add x1, x9, 56
+    add x2, x10, 56
+    add x3, x9, 69
+    add x4, x10, 62
     bl rectangulo
 
-    mov x0, x20
-    mov x1, 56           //x_1
-    mov x2, 63           //y_1
-    mov x3, 62           //x_2
-    mov x4, 69           //y_2
-    ldr x7, =BLANCO      //color
+    add x1, x9, 56
+    add x2, x10, 63
+    add x3, x9, 62
+    add x4, x10, 69
     bl rectangulo
 
-    mov x0, x20
-    mov x1, 28           //x_1
-    mov x2, 70           //y_1
-    mov x3, 34           //x_2
-    mov x4, 76           //y_2
-    ldr x7, =BLANCO      //color
+    add x1, x9, 28
+    add x2, x10, 70
+    add x3, x9, 34
+    add x4, x10, 76
     bl rectangulo
 
-    mov x0, x20
-    mov x1, 42           //x_1
-    mov x2, 70           //y_1
-    mov x3, 48           //x_2
-    mov x4, 76           //y_2
-    ldr x7, =BLANCO      //color
+    add x1, x9, 42
+    add x2, x10, 70
+    add x3, x9, 48
+    add x4, x10, 76
     bl rectangulo
 
+    LDR X30, [SP,#40]
     RET
-    
