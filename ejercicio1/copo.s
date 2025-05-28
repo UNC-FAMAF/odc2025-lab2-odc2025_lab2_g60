0305c73a2b7f9a97aa9e1a8b421e45f49191747d
manug859 <manu.gomez859@gmail.com>
2025-05-27 22:46:17 -0300

    actualizado el tamaño del rectángulo en la función copo y reubicadas las inclusiones de archivos

--- ejercicio1/copo.s
+++ ejercicio1/copo.s
@@ -1,4 +1,6 @@
     .text
+    .include "colores.s"
+    .include "rectangulo.s"
     .globl copo
 
 copo:
@@ -6,8 +8,8 @@ copo:
     mov x0, x20
     mov x1, 0
     mov x2, 0
-    mov x3, 1
-    mov x4, 5
+    mov x3, 100
+    mov x4, 100
     ldr x7, =BLANCO
     bl pintar_rectangulo
 
