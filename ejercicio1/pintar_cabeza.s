c32019b4e9a2ca2f4d90d00a47a06cbe6fab047a
ginafantinii <149610824+ginafantinii@users.noreply.github.com>
2025-05-28 21:36:33 -0300

    Update pintar_cabeza.s

--- ejercicio1/pintar_cabeza.s
+++ ejercicio1/pintar_cabeza.s
@@ -585,7 +585,153 @@ mov x4, 218
 ldr x7, = GRIS_BLANCO
 bl rectangulo 
 
+mov x0, x20 
+mov x1, 306
+mov x2, 247
+mov x3, 354
+mov x4, 253
+ldr x7, = BLANCO
+bl rectangulo 
 
+mov x0, x20 
+mov x1, 299
+mov x2, 240
+mov x3, 368
+mov x4, 246
+ldr x7, = BLANCO
+bl rectangulo 
+
+mov x0, x20 
+mov x1, 292
+mov x2, 233
+mov x3, 368
+mov x4, 239
+ldr x7, = BLANCO
+bl rectangulo 
+
+mov x0, x20 
+mov x1, 285
+mov x2, 226
+mov x3, 375
+mov x4, 232
+ldr x7, = BLANCO
+bl rectangulo 
+
+mov x0, x20 
+mov x1, 285
+mov x2, 219
+mov x3, 375
+mov x4, 225
+ldr x7, = BLANCO
+bl rectangulo 
+
+mov x0, x20 
+mov x1, 278
+mov x2, 212
+mov x3, 382
+mov x4, 218
+ldr x7, = BLANCO
+bl rectangulo 
+
+mov x0, x20 
+mov x1,191 
+mov x2, 278
+mov x3, 382
+mov x4, 218
+ldr x7, = BLANCO
+bl rectangulo 
+
+mov x0, x20 
+mov x1, 375
+mov x2, 190
+mov x3, 313
+mov x4, 177
+ldr x7, = BLANCO
+bl rectangulo 
+
+mov x0, x20 
+mov x1, 313
+mov x2, 170
+mov x3, 368
+mov x4, 176
+ldr x7, = BLANCO
+bl rectangulo 
+
+
+//BOCA
+mov x0, x20 
+mov x1, 299
+mov x2, 219
+mov x3, 305
+mov x4, 225
+ldr x7, = NEGRO
+bl rectangulo
+
+mov x0, x20 
+mov x1, 313
+mov x2, 226
+mov x3, 319
+mov x4, 232
+ldr x7, = NEGRO 
+bl rectangulo
+
+mov x0, x20 
+mov x1, 327
+mov x2, 226
+mov x3, 333
+mov x4, 232
+ldr x7, = NEGRO 
+bl rectangulo
+
+mov x0, x20 
+mov x1, 341
+mov x2, 226
+mov x3, 347
+mov x4, 232
+ldr x7, = NEGRO 
+bl rectangulo
+
+mov x0, x20 
+mov x1, 355
+mov x2, 219
+mov x3, 361
+mov x4, 225
+ldr x7, = NEGRO 
+bl rectangulo
+
+//NARIZ
+mov x0, x20 
+mov x1, 327
+mov x2, 205
+mov x3, 382
+mov x4, 211
+ldr x7, = NARANJA 
+bl rectangulo
+
+mov x0, x20 
+mov x1, 334
+mov x2, 198
+mov x3, 368
+mov x4, 204
+ldr x7, = NARANJA
+bl rectangulo
+
+//OJOS 
+mov x0, x20 
+mov x1, 299
+mov x2, 170
+mov x3, 312
+mov x4, 197
+ldr x7, = NEGRO 
+bl rectangulo
+
+mov x0, x20 
+mov x1, 355
+mov x2, 170
+mov x3, 368
+mov x4, 197
+ldr x7, = NEGRO 
+bl rectangulo
   LDR X30, [SP,#40]
  RET 
 
